39ddf1a4ba7ebdf556bad8511add56cc6ce63657
Alina232323 <alina.polianska@vergesense.com>
2023-04-05 21:37:42 -0700

    added links to plants on index page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,11 @@ import { Route, Routes } from "react-router-dom";
 
 //Import pages
 import Index from "./pages/Index";
+import Show from "./pages/Show";
 
 function App() {
 
+  
   // const URL = "https://plantdatabase.herokuapp.com/";
   const URL = "https://perenual.com/api/species-list?page=1&key=sk-3t2R642df04b75c19417";
 
@@ -16,6 +18,7 @@ function App() {
 
      <Routes>
         <Route exact path="/index" element={<Index URL={URL}/>}/> 
+        <Route exact path="/show/:id" element={<Show/>}/> 
         
       </Routes>
       
