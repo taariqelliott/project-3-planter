7fd74069d3b07612465515622f59c19bf51fe31d
Alina232323 <alina.polianska@vergesense.com>
2023-04-06 18:20:04 -0700

    set up blogs show

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,28 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 
+
 //Import pages
 import Index from "./pages/Index";
 import Show from "./pages/Show";
+import Home from "./pages/Home";
+import BlogsShow from "./pages/BlogsShow";
 
 function App() {
 
   
-  // const URL = "https://plantdatabase.herokuapp.com/";
-  const URL = "https://perenual.com/api/species-list?page=1&key=sk-3t2R642df04b75c19417";
+  const URL = "https://plantdatabase.herokuapp.com/";
+  // const URL = "https://perenual.com/api/species-list?page=1&key=sk-3t2R642df04b75c19417";
 
   return (
     <div className="flex-box">
 
      <Routes>
+
+        <Route exact path="/" element={<Home/>}/> 
         <Route exact path="/index" element={<Index URL={URL}/>}/> 
         <Route exact path="/show/:id" element={<Show/>}/> 
+        <Route exact path="/blogs" element={<BlogsShow />}/> 
         
       </Routes>
       
---
+++ frontend/src/pages/BlogsShow.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+const BlogsShow = (props) => {
+    return <h1>Blogs Show page
+    </h1>
+    
+}
+
+export default BlogsShow
