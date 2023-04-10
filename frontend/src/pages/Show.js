76166d52e1110f36c4fd77c0656c01f209b99ae5
trees-arent-sleeping <transferdata76@gmail.com>
2023-04-10 13:55:44 -0700

    changed show info, styled show and index, excluded searchbar from flex, search clears on backspace

--- frontend/src/pages/Show.js
+++ frontend/src/pages/Show.js
@@ -23,105 +23,97 @@ function Show(props) {
   }, [id]);
 
   return (
-    <div>
+    <div className="plant-details">
       {plant ? (
         <div>
           <div>
-            <img src={plant.default_image.small_url} alt={plant.common_name} />
+            <img
+              className="plant-image"
+              src={plant.default_image.small_url}
+              alt={plant.common_name}
+            />
             <h1>{plant.common_name}</h1>
             <h2>
               <em>{plant.scientific_name}</em>
             </h2>
-            {plant.other_name && (
-              <p>
-                <strong>Other Names:</strong> {plant.other_name.join(", ")}
-              </p>
-            )}
-            {plant.family && (
-              <p>
-                <strong>Family:</strong> {plant.family}
-              </p>
-            )}
-            {plant.origin && (
+
+            <p>
+              <strong>Description:</strong> {plant.description}
+            </p>
+
+            <p>
+              <strong>Type:</strong> {plant.type}
+            </p>
+
+            {plant.origin && plant.origin.length > 0 && (
               <p>
                 <strong>Origin:</strong> {plant.origin.join(", ")}
               </p>
             )}
-            {plant.type && (
-              <p>
-                <strong>Type:</strong> {plant.type}
-              </p>
-            )}
-            {plant.dimension && (
-              <p>
-                <strong>Dimension:</strong> {plant.dimension}
-              </p>
-            )}
-            {plant.cycle && (
-              <p>
-                <strong>Cycle:</strong> {plant.cycle}
-              </p>
-            )}
-            {plant.watering && (
-              <p>
-                <strong>Watering:</strong> {plant.watering}
-              </p>
-            )}
-            {plant.growth_rate && (
-              <p>
-                <strong>Growth Rate:</strong> {plant.growth_rate}
-              </p>
-            )}
-            {plant.hardiness && (
-              <p>
-                <strong>Hardiness:</strong> {plant.hardiness.min}-
-                {plant.hardiness.max}
-              </p>
-            )}
-            {plant.sunlight && (
+
+            <p>
+              <strong>Growth Rate:</strong> {plant.growth_rate}
+            </p>
+
+            <p>
+              <strong>Dimension:</strong> {plant.dimension}
+            </p>
+
+            <p>
+              <strong>Cycle:</strong> {plant.cycle}
+            </p>
+
+            <p>
+              <strong>Watering:</strong> {plant.watering}
+            </p>
+
+            <p>
+              <strong>Hardiness:</strong>{" "}
+              {plant.hardiness &&
+                `${plant.hardiness.min}-${plant.hardiness.max}`}
+            </p>
+
+            {plant.sunlight && plant.sunlight.length > 0 && (
               <p>
                 <strong>Sunlight:</strong> {plant.sunlight.join(", ")}
               </p>
             )}
-            {plant.soil.length > 0 && (
+
+            {plant.soil && plant.soil.length > 0 && (
               <p>
                 <strong>Soil:</strong> {plant.soil.join(", ")}
               </p>
             )}
+
+            <p>
+              <strong>Tropical:</strong> {plant.tropical ? "Yes" : "No"}
+            </p>
+
+            <p>
+              <strong>Invasive:</strong> {plant.invasive ? "Yes" : "No"}
+            </p>
+
+            {plant.propagation && plant.propagation.length > 0 && (
+              <p>
+                <strong>Propagation:</strong> {plant.propagation.join(", ")}
+              </p>
+            )}
+
             {plant.maintenance && (
               <p>
                 <strong>Maintenance:</strong> {plant.maintenance}
               </p>
             )}
-            {plant.care_level && (
+
+            {plant.other_name && plant.other_name.length > 0 && (
               <p>
-                <strong>Care Level:</strong> {plant.care_level}
+                <strong>Other Names:</strong> {plant.other_name.join(", ")}
               </p>
             )}
-            {plant.tropical !== undefined && (
+
+            {plant.family && (
               <p>
-                <strong>Tropical:</strong> {plant.tropical ? "Yes" : "No"}
-              </p>
-            )}
-            {plant.invasive !== undefined && (
-              <p>
-                <strong>Invasive:</strong> {plant.invasive ? "Yes" : "No"}
-              </p>
-            )}
-            {plant.description && (
-              <p>
-                <strong>Description:</strong> {plant.description}
-              </p>
-            )}
-            {plant.propagation && (
-              <p>
-                <strong>Propagation:</strong> {plant.propagation.join(", ")}
-              </p>
-            )}
-            {plant.default_image.license_name && (
-              <p>
-                <strong>Default Image License:</strong>{" "}
-                {plant.default_image.license_name}
+                <strong>Family:</strong> {plant.family}
               </p>
             )}
           </div>
