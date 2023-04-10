e471f32529b075e980e62e1197ffae7aee67bdc7
trees-arent-sleeping <transferdata76@gmail.com>
2023-04-10 12:03:39 -0700

    a

--- backend/controllers/blogsCtrls.js
+++ backend/controllers/blogsCtrls.js
@@ -1,60 +1,55 @@
-const db = require('../models')
-console.log(db)
-
+const db = require("../models");
+console.log(db);
 
 //getblogs
 const getBlogs = (req, res) => {
-	db.Blogs.find({})
-	.then((foundBlogs) => {
-		if(!foundBlogs) {
-			res.status(404).json({message: "Cannot find Blogs"})
-		} else {
-			res.status(200).json({data: foundBlogs})
-		}
-	})
-}
-
+  db.Blogs.find({}).then((foundBlogs) => {
+    if (!foundBlogs) {
+      res.status(404).json({ message: "Cannot find Blogs" });
+    } else {
+      res.status(200).json({ data: foundBlogs });
+    }
+  });
+};
 
 //create blogs
 const createBlogs = (req, res) => {
-	db.Blogs.create(req.body)
-	.then((createdBlog) => {
-		if(!createdBlog) {
-			res.status(404).json({message: "Cannot create Blog"})
-		} else {
-			res.status(201).json({data: createdBlog})
-		}
-	})
-}
+  db.Blogs.create(req.body).then((createdBlog) => {
+    if (!createdBlog) {
+      res.status(404).json({ message: "Cannot create Blog" });
+    } else {
+      res.status(201).json({ data: createdBlog });
+    }
+  });
+};
 
 //Update blogs
 const updateBlog = (req, res) => {
-	db.Blogs.findByIdAndUpdate(req.params.id, req.body, {new: true})
-	.then((updatedBlog) => {
-	if(!updatedBlog) {
-			res.status(400).json({message: "Cannot update Blog"})
-		} else {
-			res.status(200).json({data: updatedPerson, message: 'updated Blog'})
-		}
-	})
-}
-
+  db.Blogs.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+    (updatedBlog) => {
+      if (!updatedBlog) {
+        res.status(400).json({ message: "Cannot update Blog" });
+      } else {
+        res.status(200).json({ data: updatedBlog, message: "updated Blog" });
+      }
+    }
+  );
+};
 
 //delete blogs
 const deleteBlog = (req, res) => {
-	db.Blogs.findByIdAndDelete(req.params.id)
-	.then((deletedBlog) => {
-		if(!deletedBlog) {
-			res.status(400).json({message: "Could not delete Blog"})
-		} else {
-			res.status(200).json({data:deletedBlog, message: 'deleted Blog'})
-		}
-	})
-}
+  db.Blogs.findByIdAndDelete(req.params.id).then((deletedBlog) => {
+    if (!deletedBlog) {
+      res.status(400).json({ message: "Could not delete Blog" });
+    } else {
+      res.status(200).json({ data: deletedBlog, message: "deleted Blog" });
+    }
+  });
+};
 
 module.exports = {
-	getBlogs,
-	createBlogs,
-	updateBlog,
-	deleteBlog
-}
+  getBlogs,
+  createBlogs,
+  updateBlog,
+  deleteBlog,
+};
--- frontend/src/pages/Show.js
+++ frontend/src/pages/Show.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-
 function Show(props) {
   const { id } = useParams();
   const [plant, setDetails] = useState(null);
@@ -33,16 +32,100 @@ function Show(props) {
             <h2>
               <em>{plant.scientific_name}</em>
             </h2>
-            <p>Type: {plant.type}</p>
-            <p>{plant.dimension}</p>
-            <p>
-              Hardiness: {plant.hardiness.min}-{plant.hardiness.max}
-            </p>
-            <p>Watering: {plant.watering}</p>
-            <p>Sunlight: {plant.sunlight.join(", ")}</p>
-            <p>Propagation: {plant.propagation.join(", ")}</p>
+            {plant.other_name && (
+              <p>
+                <strong>Other Names:</strong> {plant.other_name.join(", ")}
+              </p>
+            )}
+            {plant.family && (
+              <p>
+                <strong>Family:</strong> {plant.family}
+              </p>
+            )}
+            {plant.origin && (
+              <p>
+                <strong>Origin:</strong> {plant.origin.join(", ")}
+              </p>
+            )}
+            {plant.type && (
+              <p>
+                <strong>Type:</strong> {plant.type}
+              </p>
+            )}
+            {plant.dimension && (
+              <p>
+                <strong>Dimension:</strong> {plant.dimension}
+              </p>
+            )}
+            {plant.cycle && (
+              <p>
+                <strong>Cycle:</strong> {plant.cycle}
+              </p>
+            )}
+            {plant.watering && (
+              <p>
+                <strong>Watering:</strong> {plant.watering}
+              </p>
+            )}
+            {plant.growth_rate && (
+              <p>
+                <strong>Growth Rate:</strong> {plant.growth_rate}
+              </p>
+            )}
+            {plant.hardiness && (
+              <p>
+                <strong>Hardiness:</strong> {plant.hardiness.min}-
+                {plant.hardiness.max}
+              </p>
+            )}
+            {plant.sunlight && (
+              <p>
+                <strong>Sunlight:</strong> {plant.sunlight.join(", ")}
+              </p>
+            )}
+            {plant.soil.length > 0 && (
+              <p>
+                <strong>Soil:</strong> {plant.soil.join(", ")}
+              </p>
+            )}
+            {plant.maintenance && (
+              <p>
+                <strong>Maintenance:</strong> {plant.maintenance}
+              </p>
+            )}
+            {plant.care_level && (
+              <p>
+                <strong>Care Level:</strong> {plant.care_level}
+              </p>
+            )}
+            {plant.tropical !== undefined && (
+              <p>
+                <strong>Tropical:</strong> {plant.tropical ? "Yes" : "No"}
+              </p>
+            )}
+            {plant.invasive !== undefined && (
+              <p>
+                <strong>Invasive:</strong> {plant.invasive ? "Yes" : "No"}
+              </p>
+            )}
+            {plant.description && (
+              <p>
+                <strong>Description:</strong> {plant.description}
+              </p>
+            )}
+            {plant.propagation && (
+              <p>
+                <strong>Propagation:</strong> {plant.propagation.join(", ")}
+              </p>
+            )}
+            {plant.default_image.license_name && (
+              <p>
+                <strong>Default Image License:</strong>{" "}
+                {plant.default_image.license_name}
+              </p>
+            )}
           </div>
-            <Link to={"/mycollections"} key={plant.id}>
+          <Link to={"/mycollections"} key={plant.id}>
             <button>Add to my collection</button>
           </Link>
         </div>
