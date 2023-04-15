2e25af48e2d69d9c896de7cd39640df556e5fb20
Alina232323 <alina.polianska@vergesense.com>
2023-04-14 21:47:08 -0700

    added delete buttons

--- frontend/src/pages/Collections.js
+++ frontend/src/pages/Collections.js
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Collections ({collection}) {
 
- 
-  console.log("collection in collection page", collection)
- 
-
+console.log(collection)
     return (
       <div>
         <h1>Collections Page</h1>
+       
         {collection.map(plant => (
-          <div key={plant.id}>
+          <div className="grid-container" key={plant.id}>
+             <button onClick={localStorage.removeItem(plant.id)}>delete</button>
             <h1>{plant.common_name}</h1>
+            <Link to={`/show/${plant.id}`} key={plant.id}>
             <img src={plant.default_image.small_url} alt={plant.default_image.medium_url} />
+          </Link>
           </div>
           ))}
       </div>
