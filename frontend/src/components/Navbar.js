6ce8719c7a77d9f6e63448322976d28b3668beba
rezwanatechProfile <rezwanatech@gmail.com>
2023-04-15 14:32:49 -0400

    Nav bar fixed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useRef} from "react"
 import { FaBars, FaTimes } from "react-icons/fa";
 
+
 const Navbar = (props) => {
 
   const navRef = useRef();
@@ -12,23 +13,28 @@ const Navbar = (props) => {
 
   return (
     <header className="item">
-      <h3>Logo</h3>
+      <div className="logo-div"><h3>Logo</h3></div>
       <nav className="nav" ref={navRef}>
-        <Link to="/plants">
+        {/* <Link to="/plants">
           <div>Home</div>
-        </Link>
+        </Link> */}
+        <a href="/">Home</a>
+        <a href="/plants">All Plants</a>
+        <a href="/mycollection">My Collection</a>
+        <a href="/new">Create Blog</a>
+        <a href="/new">Sign in</a>
 
-        <Link to="/mycollection">
+        {/* <Link to="/mycollection">
           <div>My Collections</div>
-        </Link>
-
+        </Link> */}
+{/* 
         <Link to="/new">
-          <div>Create Blog</div>
+          <div>Blog</div>
         </Link>
 
         <Link to="/#">
           <div>Sign in</div>
-        </Link>
+        </Link> */}
 
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
