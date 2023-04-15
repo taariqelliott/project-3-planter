2f67fb6f63b235988a715758141ed156f6b73f6f
Alina232323 <alina.polianska@vergesense.com>
2023-04-15 10:49:02 -0700

    added toggle for delete btns

--- frontend/src/pages/Collections.js
+++ frontend/src/pages/Collections.js
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { useState} from "react";
+
 
 function Collections ({collection}) {
+
+  const [toggle, setToggle] = useState(false)
 
   const handleDelete = (id) => {
     // array to include plants with different id's from the one provided in handleDelete
@@ -8,24 +12,32 @@ function Collections ({collection}) {
     localStorage.setItem('collection', JSON.stringify(updatedCollection))
   }
 
-
 console.log(collection)
     return (
       <div>
         <h1>Collections Page</h1>
-       <div className="grid-container">
-        {collection.map(plant => (
-              <div className="plantContainer" key={plant.id}>
-          
-             <button onClick={() => {handleDelete(plant.id); console.log("deleted plant with id:", plant.id)}}>delete</button>
-            <h3>{plant.common_name}</h3>
-            <Link to={`/show/${plant.id}`} key={plant.id}>
-            <img src={plant.default_image.small_url} alt={plant.default_image.medium_url} />
-            
-          </Link>
-          </div>
-          ))}
-          </div>
+            <button 
+              onClick={() => setToggle(!toggle)} 
+              className="collections-page-btn ">
+              Select and Delete Plants
+            </button>
+              <div className="grid-container">
+                {collection.map(plant => (
+                  <div className="plantContainer" key={plant.id}>
+                    {toggle && (
+                      <div>
+                        <button className="collections-page-btn" onClick={() => {
+                          handleDelete(plant.id); 
+                          console.log("deleted plant with id:", plant.id)}}>delete</button>
+                      </div>
+                    )}
+                      <h3>{plant.common_name}</h3>
+                        <Link to={`/show/${plant.id}`} key={plant.id}>
+                          <img src={plant.default_image.small_url} alt={plant.default_image.medium_url} />
+                        </Link>
+                  </div>
+                ))}
+              </div>
       </div>
     )
 }
