79157e5d98c87aa043cff1a9c5030da15f204696
Alina232323 <alina.polianska@vergesense.com>
2023-04-15 08:02:00 -0700

    fixed delete button for collections

--- frontend/src/pages/Collections.js
+++ frontend/src/pages/Collections.js
@@ -2,6 +2,12 @@ import { Link } from "react-router-dom";
 
 function Collections ({collection}) {
 
+  const handleDelete = (id) => {
+    const updatedCollection = collection.filter(plant => plant.id !== id)
+    localStorage.setItem('collection', JSON.stringify(updatedCollection))
+  }
+
+
 console.log(collection)
     return (
       <div>
@@ -9,7 +15,7 @@ console.log(collection)
        
         {collection.map(plant => (
           <div className="grid-container" key={plant.id}>
-             <button onClick={localStorage.removeItem(plant.id)}>delete</button>
+             <button onClick={() => {handleDelete(plant.id); console.log("deleted plant with id:", plant.id)}}>delete</button>
             <h1>{plant.common_name}</h1>
             <Link to={`/show/${plant.id}`} key={plant.id}>
             <img src={plant.default_image.small_url} alt={plant.default_image.medium_url} />
