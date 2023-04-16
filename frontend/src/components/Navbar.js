5034534f36857fdd149e166285b7b6bdaa4658aa
Alina232323 <alina.polianska@vergesense.com>
2023-04-16 11:30:34 -0700

    logout and my collection buttons are not visible if user is not logged in

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -21,10 +21,12 @@ const Navbar = (props) => {
       <nav className="nav" ref={navRef}>
         <a href="/">Home</a>
         <a href="/plants">All Plants</a>
-        <a href="/mycollections">My Collection</a>
         <a href="/new">Create Blog</a>
           {isAuthenticated ? (
+            <>
+            <a href="/mycollections">My Collection</a>
             <a href="/logout" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</a>
+            </>
           ):(
             <a href="/login" onClick={() => loginWithRedirect()}>Log in</a>
           )}
