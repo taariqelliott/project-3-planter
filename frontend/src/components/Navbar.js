1475b58f7949afde26c9b808386d0a2d708574a0
rezwanatechProfile <rezwanatech@gmail.com>
2023-04-17 22:50:29 -0400

    navbar modified

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -23,17 +23,19 @@ const Navbar = (props) => {
   return (
     <header className="item">
       <div className="logo-div"><a href="/"><img className="plantlogo" src="https://i.imgur.com/HqoLHeR.png" alt="" /></a></div>
+      <button className="nav-btn" onClick={showNavbar}>
+          <FaBars />
+      </button>
       
       <nav className="nav" ref={navRef}>
 
-        <a href="/">Home</a>
         <a href="/plants">All Plants</a>
 
           {isAuthenticated ? (
             <>
         <a href="/new">Create Blog</a>
         <a href="/mycollections" >My Collection</a>
-            <a href="/logout" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</a>
+        <a href="/logout" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</a>
           </>
           ):(
             <>
@@ -67,9 +69,7 @@ const Navbar = (props) => {
           <FaTimes />
         </button>
       </nav>
-      <button className="nav-btn" onClick={showNavbar}>
-          <FaBars />
-      </button>
+      
     </header>
   );
 };
