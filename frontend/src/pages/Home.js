79d6186fdd9baf63a11447cd5d7f57c030c1dd61
Taariq Elliott <taariq.elliott@yahoo.com>
2023-04-20 16:50:45 -0700

    added home styling and button

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,25 @@
 function Home(props) {
-
   return (
-    <div>
-      <h1>Welcome to the Emergency Room of plants</h1>
+    <div className="homeParent">
+      <div className="videoWrapper">
+        <iframe
+          src="https://www.youtube.com/embed/CV2P-xsEiYE?autoplay=1&mute=1&loop=1&playlist=CV2P-xsEiYE"
+          frameborder="0"
+          allowFullScreen={true}
+        ></iframe>
+      </div>
+      <div className="homeChild">
+        <h1>plant(ER)</h1>
+        <h5 className="homedesc">The Virtual Emergency Room For Plants</h5>
+        <a href="/plants">
+          <button className="homebutton">
+            <h4>Get Started</h4>
+          </button>
+        </a>
+      </div>
     </div>
-  )
-  
+  );
 }
+
 
 export default Home;
