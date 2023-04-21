cd3d5d26610cdd27aa14c6c1c262d8ec135b87c3
Alina232323 <alina.polianska@vergesense.com>
2023-04-20 22:37:05 -0700

    changed to the home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 function Home(props) {
   return (
-    <div className="homeParent">
+    <div className="homeParent1">
       <div className="videoWrapper">
         <video className="back-video" id="background-video" autoPlay loop muted video="https://planter123.s3.us-east-2.amazonaws.com/flowers1.mp4">
         <source src="https://planter123.s3.us-east-2.amazonaws.com/flowers1.mp4" type="video/mp4"/>
